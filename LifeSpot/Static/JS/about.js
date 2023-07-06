fcfe8d0da4627955a609a8f46d1016e021e71c17
Ekrador <101429944+Ekrador@users.noreply.github.com>
2023-07-07 00:17:40 +0500

    js 30 module

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,23 +1,52 @@
 ﻿function askForReview() {
-    let review = {}
-    review["userName"] = prompt("Как вас зовут ?")
-    if (review["userName"] == null) {
-        return
+    this.userName = prompt("Как вас зовут ?")
+    if (this.userName == null) {
+        this.empty = true;
+        return;
     }
 
-    review["comment"] = prompt("Напишите свой отзыв")
-    if (review["comment"] == null) {
-        return
+    this.comment = prompt("Напишите свой отзыв")
+    if (this.comment == null) {
+        this.empty = true;
+        return;
     }
 
-    review["date"] = new Date().toLocaleString()
-
-    postReview(review)
+    this.date = new Date().toLocaleString();
 }
 
-const postReview = review => { 
+function addComment() {
+    let comment = new askForReview();
+    if (comment.empty) {
+        return;
+    }
+
+    if (confirm("Желаете дать другим посетителям возможность оценивать ваш комментарий?")) {
+        let ratedComment = Object.create(comment);
+        ratedComment.rate = 0;
+        postReview(ratedComment);
+    }
+    else {
+        postReview(comment);
+    }
+}
+
+const postReview = review => {
+    let rating = '';
+    
+    if (review.hasOwnProperty('rate')) {
+        rating += `           <b style="color: chocolate">Рейтинг: <button id =“${review.userName + review.date}” onclick = "addLike(this.id)">❤️ ${review.rate}</button> </b>   `;
+    }
     document.getElementsByClassName('reviews')[0].innerHTML += '<div class="review-text">\n' +
-    `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
-    `<p>${review['comment']}</p>` +
-    '</div>';
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']}${rating}</i></p>` +
+        `<p>${review['comment']}</p>` +
+        '</div>';
+}
+
+function addLike(id) {
+    let like = document.getElementById(id)
+    let array = like.innerText.split(' ')
+    let resultNum = parseInt(array[array.length - 1], 10);
+    resultNum += 1
+    array[array.length - 1] = `${resultNum}`
+    like.innerText = array.join(' ')
 }
--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,22 +1,42 @@
-﻿let session = new Map();
-
-function checkAge() {
-    session.set("age", prompt("Введите ваш возраст"));
-    if (session.get("age") < 18)
-        window.location.href = "http://www.google.com"
-    else 
-        alert("Приветствуем на LifeSpot, текущее время: " + new Date().toLocaleString());
-}
-
-function handleSession() {
-    session.set("startDate", new Date().toLocaleString())
-    session.set("userAgent", window.navigator.userAgent)
-}
-
-let sessionLog = function sessionLoger() {
-    for (let userInfo of session) {
-        console.log(userInfo)
+﻿let checker = function (newVisit) {
+    if (window.sessionStorage.getItem("userAge") >= 18) {
+        if (newVisit) {
+            alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+        }
     }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+function handleSession(logger, checker) {
+
+    if (window.sessionStorage.getItem("startDate") == null) {
+        window.sessionStorage.setItem("startDate", new Date().toLocaleString())
+    }
+
+    if (window.sessionStorage.getItem("userAgent") == null) {
+        window.sessionStorage.setItem("userAgent", window.navigator.userAgent)
+    }
+
+    if (window.sessionStorage.getItem("userAge") == null) {
+        let input = prompt("Пожалуйста, введите ваш возраст");
+        window.sessionStorage.setItem("userAge", input)
+
+        checker(true)
+    } else {
+
+        checker(false)
+    }
+
+    logger()
+}
+
+let logger = function () {
+    console.log('Начало сессии: ' + window.sessionStorage.getItem("startDate"))
+    console.log('Даныне клиента: ' + window.sessionStorage.getItem("userAgent"))
+    console.log('Возраст пользователя: ' + window.sessionStorage.getItem("userAge"))
 }
 
 const userInput = function () {
